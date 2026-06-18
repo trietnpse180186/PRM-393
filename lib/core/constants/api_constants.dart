4bda3acaa105efb677b61312ec6ec5d1e3b3452b
Triet <trietnpse180186@fpt.edu.vn>
2026-06-18 11:34:18 +0700

    fix flutter backend connection config

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/foundation.dart';
 
 class ApiConstants {
+  static const String _apiBaseUrl = String.fromEnvironment('API_BASE_URL');
+
   static String get baseUrl {
+    if (_apiBaseUrl.isNotEmpty) {
+      return _apiBaseUrl;
+    }
+
     if (kIsWeb) {
       return "http://localhost:5000";
     }
-    // Connect to the computer's current local Wi-Fi IP address
-    return "http://10.87.33.118:5000";
+
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return "http://10.0.2.2:5000";
+    }
+
+    return "http://localhost:5000";
   }
 
   static String get login => "$baseUrl/api/users/login";
